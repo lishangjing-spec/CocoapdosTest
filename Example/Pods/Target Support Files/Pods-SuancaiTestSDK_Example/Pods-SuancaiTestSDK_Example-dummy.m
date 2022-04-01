cc81318d00035a998c87078029909479c67eba36
酸菜 <>
2022-04-01 11:22:54 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-SuancaiTestSDK_Example/Pods-SuancaiTestSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SuancaiTestSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SuancaiTestSDK_Example
+@end
---
+++ Example/Pods/Target Support Files/SuancaiTestSDK/SuancaiTestSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SuancaiTestSDK : NSObject
+@end
+@implementation PodsDummy_SuancaiTestSDK
+@end
